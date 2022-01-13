fe6023c56d0956a88b4c91ab6048af5f46e3c66d
unknown <ghaliarose89@gmail.com>
2022-01-13 12:23:54 -0600

    profile

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -2,6 +2,10 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import Auth from '../../utils/auth';
 const Header = () => {
+  const logout = event => {
+    event.preventDefault();
+    Auth.logout();
+  };
   return (
     <header className="bg-secondary mb-4 py-2 flex-row align-center">
       <div className="container flex-row justify-space-between-lg justify-center align-center">
@@ -12,7 +16,7 @@ const Header = () => {
           {Auth.loggedIn() ? (
             <>
               <Link to="/profile">Me</Link>
-              <a href="/">
+              <a href="/" onClick={logout}>
                 Logout
               </a>
             </>
